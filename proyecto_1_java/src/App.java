0dee965e68daa60faa562018f83652dcf818710e
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2024-04-02 19:43:53 -0500

    Cambio realizado

--- proyecto_1_java/src/App.java
+++ proyecto_1_java/src/App.java
@@ -14,6 +14,19 @@ public class App {
         while(true){
             System.out.println("----------Menu de usuarios---------");
             
+
+
+
+
+
+
+
+
+
+
+
+
+            
             System.out.println("1. Agregar clientes");
             System.out.println("2. Listar clientes");
             System.out.println("3. Buscar clientes");
